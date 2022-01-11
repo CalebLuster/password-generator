11bfa1314914153172923592961e8f268d7db9ac
Caleb Luster <cwl117@outlook.com>
2022-01-10 21:24:11 -0600

    continued to work out kinks. added password length function

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,133 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-const passwordMost = 128;
-const passwordLeast = 8;
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "1234567890";
-const symbols = "`~!@#$%^&*()_+=-,<.>/?';:[{]}|";
+
+var passwordMost = 128;
+var passwordLeast = 8;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var symbols = "`~!@#$%^&*()_+=-,<.>/?';:[{]}|";
 
 
 
 
 
-function generatePassword() {
-  console.log("button clicked")
 
   // 1. create prompts. 
-  
-  window.prompt("How long is your password? (Select a number between 8 - 128.)")
 
-  window.prompt("Do you want uppercase letters in your password?")
-
-  window.prompt("Do you want lowercase letters in your password?")
-
-  window.prompt("Do you want numbers in your password?")
-
-  window.prompt("Do you want symbols in your password?")
+  /**
+   *    PROMPTS IN README FILE
+   */
 
   //  a. Password length has min of 8 and max of 128
 
-function PasswordLength () {
-  var passwordLength = 0;
-  while (passwordLength < passwordLength || passwordLength > passwordMost) {
-    window.prompt("How long is your password? (Select a number between 8 - 128.)")
+
+  // WORKING CODE
+  function getPasswordLength () {
+    var PasswordLength = 0;
+    while (PasswordLength < passwordLeast || PasswordLength > passwordMost) {
+      PasswordLength = prompt("Please choose how long you want your password. You can pick between " + (passwordLeast) + ' - ' + (passwordMost) + '.');
+      if (PasswordLength > passwordMost) {
+        alert("Please keep your password within " + (passwordMost) + ' charecters.')
+        } else if (PasswordLength < passwordLeast) {
+          alert("Please keep your password at least " + (passwordLeast) + ' or more characters long.')
+      } 
+    }
+    return PasswordLength
   }
-}
+// WORKING CODE
+
 
   //  b. use numbers, lowercase, uppercase, or special characters options
-  // 2. validate the input.
-  // 3. generate password
-  
+  function getRandomSymbol () {
+    var getSymbols = 'yes';
+    getSymbols = prompt("Do you want symbols in your password?");
+    
 
-
-// 4. display password back to user.
-  return "Generated password here"
 }
+  function getRandomNumber () {
+
+  }
+  function getRandomUppercase () {
+    
+  }
+  function getRandomLowercase () {
+    
+  }
+
+  // 2. validate the input.
+
+
+
+  // 3. generate password
+  function generatePassword() {
+    console.log("button clicked")
+  getPasswordLength ();
+
+
+ 
+
+
+  
+  // 4. display password back to user.
+  return "I clicked the button"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -54,6 +137,11 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+  
--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
 # Password Generator Starter Code
+
+
+   window.prompt("How long is your password? (Select a number between 8 - 128.)")
+
+   window.prompt("Do you want lowercase letters in your password?")
+
+   window.prompt("Do you want numbers in your password?")
+
+   window.prompt("Do you want symbols in your password?")
+
+
+
+
+
+return symbols[Math.floor(Math.random() * symbols.length)];
+
+return numbers[Math.floor(Math.random() * numbers.length)];
+
+return upperCase[Math.floor(Math.random() * upperCase.length)];
+
+return lowerCase[Math.floor(Math.random() * lowerCase.length)];
