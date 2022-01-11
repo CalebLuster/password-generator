54b87bc845be609826abcf8975a8a383ef528ddb
Caleb Luster <cwl117@outlook.com>
2022-01-11 14:30:04 -0600

    Added most of the prompts and the selections of the characters.

--- Develop/script.js
+++ Develop/script.js
@@ -15,11 +15,6 @@ var symbols = "`~!@#$%^&*()_+=-,<.>/?';:[{]}|";
 
 
   // 1. create prompts. 
-
-  /**
-   *    PROMPTS IN README FILE
-   */
-
   //  a. Password length has min of 8 and max of 128
 
 
@@ -27,11 +22,11 @@ var symbols = "`~!@#$%^&*()_+=-,<.>/?';:[{]}|";
   function getPasswordLength () {
     var PasswordLength = 0;
     while (PasswordLength < passwordLeast || PasswordLength > passwordMost) {
-      PasswordLength = prompt("Please choose how long you want your password. You can pick between " + (passwordLeast) + ' - ' + (passwordMost) + '.');
+      PasswordLength = prompt("Choose how long you want your password. You can pick between a minimum of " + (passwordLeast) + ' Or a maximum of ' + (passwordMost) + '.');
       if (PasswordLength > passwordMost) {
-        alert("Please keep your password within " + (passwordMost) + ' charecters.')
+        alert("It is clearly stated in the before prompt that the maximum amount is " + (passwordMost) + ' charecters. This time, select somthing within this range PLEASE.')
         } else if (PasswordLength < passwordLeast) {
-          alert("Please keep your password at least " + (passwordLeast) + ' or more characters long.')
+          alert("It is clearly stated in the before prompt that the minimum amount is " + (passwordLeast) + ' characters. This time, select something within this range PLEASE.')
       } 
     }
     return PasswordLength
@@ -40,20 +35,64 @@ var symbols = "`~!@#$%^&*()_+=-,<.>/?';:[{]}|";
 
 
   //  b. use numbers, lowercase, uppercase, or special characters options
-  function getRandomSymbol () {
-    var getSymbols = 'yes';
-    getSymbols = prompt("Do you want symbols in your password?");
-    
 
-}
-  function getRandomNumber () {
+  function gatherInfo () {
+    var askSymbols = confirm("Select if you want to have symbols in your generated password");
+    var askNumbers = confirm("Select if you want to have numbers in your generated password");
+    var askLowercase = confirm("Select if you want to have lowercase letters in your generated password");
+    var askUppercase = confirm("Select if you want to have uppercase letters in your generated password");
+    var blank = "";
 
-  }
-  function getRandomUppercase () {
-    
-  }
-  function getRandomLowercase () {
-    
+    // PROCESS OF GENERATING PASSWORD
+
+    // ASKING FOR SYMBOLS IN PASSWORD
+
+    if (askSymbols) {
+      blank =+ symbols
+      window.alert("Adding symbols to your generated password... This may take as long as it takes for you to click the button (this isn't a real loading screen)");
+    }
+    else {
+        window.alert("Removing symbols...");
+    }
+
+    // ASKING FOR NUMBERS IN PASSWORD
+
+    if (askNumbers) {
+      blank =+ numbers
+      window.alert("Adding numbers to your generated password... This may take as long as it takes for you to click the button (this isn't a real loading screen)");
+    }
+    else {
+       window.alert("Removing numbers...");
+    }
+
+    // ASKING FOR LOWERCASE LETTERS IN PASSWORD
+
+   if (askLowercase) {
+     blank =+ lowerCase
+     window.alert("Adding lowercase letters to your generated password... This may take as long as it takes for you to click the button (this isn't a real loading screen)");
+    }
+    else {
+        window.alert("Removing lowercase letters...");
+    }
+
+    // ASKING FOR UPPERCASE LETTERS IN PASSWORD
+
+   if (askUppercase) {
+     blank =+ upperCase
+     window.alert("Adding uppercase letters to your generated password... This may take as long as it takes for you to click the button (this isn't a real loading screen)");
+   }
+   else {
+     window.alert("Removing uppercase letters...");
+   }
+
+   if (!askSymbols && !askNumbers && !askLowercase && !askUppercase) {
+      alert("You are trying to generate a password, so why did you hit no for all of the questions? Let's try this again...");
+      return gatherInfo ();
+   }
+   else if (askSymbols && askNumbers && askLowercase && askUppercase) {
+      window.alert("Wow, All variables were selected! I will generate a password much like you... EXTRA")
+   }
+   generatePassword();
   }
 
   // 2. validate the input.
@@ -62,67 +101,20 @@ var symbols = "`~!@#$%^&*()_+=-,<.>/?';:[{]}|";
 
   // 3. generate password
   function generatePassword() {
-    console.log("button clicked")
-  getPasswordLength ();
+
+    const PasswordLength;
+    const password = "";
+    const gatherInfoResults = "";
 
 
- 
-
-
+// Variables calling functions
+ PasswordLength = getPasswordLength ();
+ gatherInfoResults = gatherInfo ();
   
+
   // 4. display password back to user.
-  return "I clicked the button"
+  return password
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
